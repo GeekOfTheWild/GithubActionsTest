2f1bfb3a2babc1ed7717bac7b8121388c688384f
GotW <thealmonddestroyer@gmail.com>
2022-09-27 07:13:25 +0000

    Fixing code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const dayOfTheWeek = (date = new Date()) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    return days[date.getay()];
+    return days[date.getDay()];
 }
 
 
